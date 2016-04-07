aaf1bf370adeaad027895995d5d17d9418ba7c47
leli <sp_hrz@qq.com>
2016-04-07 13:43:45 +0800

    2016-04-07

--- src/main/java/cn/accessbright/blade/web/controller/HomeController.java
+++ src/main/java/cn/accessbright/blade/web/controller/HomeController.java
@@ -3,6 +3,8 @@ package cn.accessbright.blade.web.controller;
 import cn.accessbright.blade.domain.system.Menu;
 import cn.accessbright.blade.repository.MenuRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,10 +24,8 @@ public class HomeController {
 
         model.addAttribute("firstLevelMenus", firstLevelMenus);
 
-
         Msg msg = new Msg("测试标题", "测试内容", "额外信息，只对管理员显示");
         model.addAttribute("msg", msg);
         return "index";
     }
-
 }
